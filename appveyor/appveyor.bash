780a8c6966ba328492956a26f5a25c6522d7734d
Oxore <oxore@protonmail.com>
2019-11-28 03:38:03 +0300

    Move build steps to separate bash script and invoke with cygwin

---
+++ appveyor/appveyor.bash
@@ -0,0 +1,4 @@
+mkdir lib/build-SFML
+pushd lib/build-SFML && cmake -DBUILD_SHARED_LIBS=OFF ../SFML && popd
+make -C lib/build-SFML
+make SFML_STATIC=y EXTRA_LDFLAGS="-L./lib/build-SFML/lib" EXTRA_CXXFLAGS="-isystem ./lib/SFML/include" Q=
